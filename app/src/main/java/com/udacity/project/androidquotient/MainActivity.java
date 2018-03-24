310eb739df52b281af2582c21dcb600b27eb4eaa
angelynaliem <angelynaliem@gmail.com>
2018-03-23 22:04:57 -0400

    Initial commit

---
+++ app/src/main/java/com/udacity/project/androidquotient/MainActivity.java
@@ -0,0 +1,27 @@
+package com.udacity.project.androidquotient;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnStartQuiz = (Button) findViewById(R.id.button_start_quiz);
+        btnStartQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), QuizActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    }
---
+++ app/src/main/java/com/udacity/project/androidquotient/QuizActivity.java
@@ -0,0 +1,25 @@
+package com.udacity.project.androidquotient;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        Button btnNextQuestion = (Button) findViewById(R.id.button_next_question);
+        btnNextQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
